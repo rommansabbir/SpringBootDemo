8bbf109f1e3029fc10e7d4e3947192355063d86a
Romman Sabbir <rommansabbir@gmail.com>
2021-10-15 20:28:14 +0600

    Base -> - APIErrors updated - APIException updated

--- src/main/kotlin/com/server/shruti/streaming/base/APIErrors.kt
+++ src/main/kotlin/com/server/shruti/streaming/base/APIErrors.kt
@@ -6,8 +6,18 @@ package com.server.shruti.streaming.base
  * Note: Any [APIException] or list of [APIException] should be populated by using this class.
  */
 object APIErrors {
+    private const val GENERIC_ERROR_CODE = 9999
+    private const val GENERIC_ERROR_MESSAGE = "Something went wrong."
     private const val DUMMY_ERROR_CODE = 1001
     private const val DUMMY_ERROR_MESSAGE = "This is just a dummy error message!!!"
 
+    private const val USER_NOT_FOUND_CODE = 1002
+    private const val USER_NOT_FOUND_MESSAGE = "User not found"
+
     val getDummyAPIException = APIException(DUMMY_ERROR_CODE, DUMMY_ERROR_MESSAGE)
+
+    val getUserNotFoundException = APIException(USER_NOT_FOUND_CODE,USER_NOT_FOUND_MESSAGE)
+
+    fun mapTopAPIException(e : Exception) = APIException(GENERIC_ERROR_CODE, e.message ?: GENERIC_ERROR_MESSAGE )
 }
+
--- src/main/kotlin/com/server/shruti/streaming/base/APIException.kt
+++ src/main/kotlin/com/server/shruti/streaming/base/APIException.kt
@@ -4,4 +4,4 @@ package com.server.shruti.streaming.base
 /**
  * This is representing API exception
  */
-data class APIException(val errorCode: Int, val message: String)
+data class APIException(val errorCode: Int, override val message: String) : Throwable()
