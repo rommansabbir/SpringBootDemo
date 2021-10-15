aedebc502a1610c6258a49ba46ed9a50b0ca91e1
Romman Sabbir <rommansabbir@gmail.com>
2021-10-15 19:24:23 +0600

    Refactoring

--- src/main/kotlin/com/server/shruti/streaming/datasource/dummy/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.server.shruti.streaming.datasource.dummy
-
-import com.server.shruti.streaming.datasource.DummyModel
-
-interface DummyRepository {
-    fun getDummyUserList(): MutableList<DummyModel>
-}
