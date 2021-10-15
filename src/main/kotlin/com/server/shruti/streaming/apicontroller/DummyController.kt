921951d13c6c525a0ca4a2c5be6fc5a9d9742d6b
Romman Sabbir <rommansabbir@gmail.com>
2021-10-15 19:22:42 +0600

    API Controller -> - Refactored

--- src/main/kotlin/com/server/shruti/streaming/apicontroller/DummyController.kt
+++ src/main/kotlin/com/server/shruti/streaming/apicontroller/DummyController.kt
@@ -1,16 +1,17 @@
-package com.server.shruti.streaming.controller
+package com.server.shruti.streaming.apicontroller
 
 import com.server.shruti.streaming.base.APIErrors
 import com.server.shruti.streaming.base.APIResponse
+import com.server.shruti.streaming.services.DummyService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/dummy") //Route for the controller
-class DummyController {
+class DummyController(private val service: DummyService) {
     @GetMapping("success") //Return a dummy success response
-    fun getDummySuccess() = APIResponse(message = "Success", data = "Hello")
+    fun getDummySuccess() = APIResponse(message = "Success", data = service.getUserList())
 
     @GetMapping("error") //Return a dummy error response with list of exceptions
     fun getDummyError() = APIResponse<Any>(
